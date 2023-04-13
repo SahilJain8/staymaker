08f868525e37260ba9a8422829891a60df0633bb
sahil <jainsahil1116@gmail.com>
2023-04-14 00:16:35 +0530

    added app hotel_info

--- hotel_info/apps.py
+++ hotel_info/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HotelDataConfig(AppConfig):
+class HotelInfoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hotel_data'
+    name = 'hotel_info'
--- hotel_info/views.py
+++ hotel_info/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(reuqest):
+    return render(reuqest,"index.html")
