d0a3478f262ac1043234cdd0d701529091346225
sahil <jainsahil1116@gmail.com>
2023-04-14 14:13:25 +0530

    new

--- login_user/views.py
+++ login_user/views.py
@@ -1,7 +1,7 @@
 
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-
+from .forms import RegisterForm
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,3 +18,17 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
