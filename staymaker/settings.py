1c88db0d2460f57ebd0f1b3cb5d315f85152cf90
sahil <jainsahil1116@gmail.com>
2023-04-14 13:40:32 +0530

    created login_user

--- staymaker/settings.py
+++ staymaker/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hotel_info'
+    'hotel_info',
+    'login_user'
     
 ]
 
