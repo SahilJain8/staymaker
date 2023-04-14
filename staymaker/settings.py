92a96d061e03360f41d3a9d9dbccfe493a3fa8b7
sahil <jainsahil1116@gmail.com>
2023-04-15 02:04:11 +0530

    few updates

--- staymaker/settings.py
+++ staymaker/settings.py
@@ -34,14 +34,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hotel_info',
-    'login_user'
+
     
 ]
 
@@ -134,4 +134,3 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'login_user.CustomUser'
