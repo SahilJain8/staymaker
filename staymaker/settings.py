2d2144f6e57383e1b3f401f27557edbdc7ba92f6
sahil <jainsahil1116@gmail.com>
2023-04-14 13:10:35 +0530

    key update

--- staymaker/settings.py
+++ staymaker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)i+v-1p3&1)#d0hzb3j25&v$3a6!q9=w*+hcy4qjdzapo5f2+'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
